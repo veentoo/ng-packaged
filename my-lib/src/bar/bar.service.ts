be19ea5b3669f4bf04e6be768a4eae305baffe86
Alexey Nagornov <al.nagornov@gmail.com>
2018-01-17 18:44:05 +0400

    reproduce issue with ramda

--- my-lib/src/bar/bar.service.ts
+++ my-lib/src/bar/bar.service.ts
@@ -3,12 +3,19 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 
+import { pipe } from 'ramda';
+
 @Injectable()
 export class BarService {
 
   get value(): Observable<string> {
+    this.ram();
     return Observable.of(true)
       .map((val) => `${val}`);
+  }
+
+  ram() {
+    pipe(() => {});
   }
 
 }
